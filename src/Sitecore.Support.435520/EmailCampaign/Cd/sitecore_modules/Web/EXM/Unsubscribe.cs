f483c78e6e352beebc6e31c4a37cda2bb9af909a
akrupskiy <anr@sitecore.net>
2020-09-11 17:27:50 +0300

    import code

---
+++ src/Sitecore.Support.435520/EmailCampaign/Cd/sitecore_modules/Web/EXM/Unsubscribe.cs
@@ -0,0 +1,9 @@
+﻿namespace Sitecore.Support.EmailCampaign.Cd.sitecore_modules.Web.EXM
+{
+    using Sitecore.Analytics;
+
+    public class Unsubscribe : Sitecore.EmailCampaign.Cd.sitecore_modules.Web.EXM.Unsubscribe
+    {
+        protected override bool IsTrackerEnabled => Tracker.Current != null;
+    }
+}
---
+++ src/Sitecore.Support.435520/EmailCampaign/Cd/sitecore_modules/Web/EXM/UnsubscribeFromAll.cs
@@ -0,0 +1,9 @@
+﻿namespace Sitecore.Support.EmailCampaign.Cd.sitecore_modules.Web.EXM
+{
+    using Sitecore.Analytics;
+
+    public class UnsubscribeFromAll : Sitecore.EmailCampaign.Cd.sitecore_modules.Web.EXM.UnsubscribeFromAll
+    {
+        protected override bool IsTrackerEnabled => Tracker.Current != null;
+    }
+}
